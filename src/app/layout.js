3f39ed4c351344283022fd9dc8c5281002954b69
Mercurial1337 <2023170473@cis.asu.edu.eg>
2026-05-28 03:01:46 +0300

    feat: add opengraph and twitter link preview images

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,10 @@ const inter = Inter({
 });
 
 export const metadata = {
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL || 
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+  ),
   title: {
     default: "Takween | Assemble your team",
     template: "%s | Takween",
